7663d48652dedf853fc82f04a09fc0ad12e4afcb
Squall <252766926@qq.com>
2020-07-23 17:18:48 +0800

    修改配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,20 @@
 import vue from 'rollup-plugin-vue'
 import { eslint } from 'rollup-plugin-eslint'
+import commonjs from '@rollup/plugin-commonjs'
 export default [
   // ESM build to be used with webpack/rollup.
   {
     input: './packages/index.js',
     output: {
-      format: 'esm',
-      file: 'dist/library.esm.js'
+      format: 'cjs',
+      file: 'dist/library.js'
     },
     plugins: [
+      commonjs(),
       vue(),
       eslint({
-        include: ['src/**/*.js'] // 需要检查的部分
+        include: ['./packages/**/*.js'] // 需要检查的部分
       })
     ]
-  },
-  // SSR build.
-  {
-    input: './packages/index.js',
-    output: {
-      format: 'cjs',
-      file: 'dist/library.ssr.js'
-    },
-    plugins: [vue({ template: { optimizeSSR: true } })]
   }
 ]
